38b7b3ff7efe443beaa72bb43b0597eb8bd2bb55
Jurriën Stutterheim <j.stutterheim@me.com>
2011-06-15 17:19:55 +0200

    Use parsinglib utils

--- src/Parser/ParseRules.hs
+++ src/Parser/ParseRules.hs
@@ -4,38 +4,30 @@ module Parser.ParseRules where
     import Text.ParserCombinators.UU
     import Text.ParserCombinators.UU.Utils
     import Text.ParserCombinators.UU.BasicInstances
+    import Text.ParserCombinators.UU.Utils
     import Parser.Base
 
     pTerm :: Parser Term
-    pTerm = (
-             pVar 
+    pTerm =
+             pVar
          <|> pLam
          <|> pApp
          <|> pLet
-            ) 
-          <* pMunch ws
 
     pVarName :: Parser String
-    pVarName = pToken "$" *> pMunch (\x -> elem x (['a'..'z']++['0'..'9']))
-
-    -- | ws tells us if a char is whitespace
-    ws :: Char -> Bool
-    ws x = elem x "\n\r\t "
+    pVarName = lexeme (pToken "$" *> pList (pLetter <|> pDigit))
 
     pVar :: Parser Term
-    pVar = Var <$> pVarName <* pMunch ws
+    pVar = Var <$> pVarName <* pSpaces
 
     pLam :: Parser Term
-    pLam = Lam <$ keyword "\\" <*> pVarName <* keyword "->" 
-               <*> pTerm <* pMunch ws
+    pLam = Lam <$ pSymbol "\\" <*> pVarName <* pSymbol "->" <*> pTerm
 
     pApp :: Parser Term
     pApp = App <$> parens pTerm <*> parens pTerm
 
     pLet :: Parser Term
-    pLet = Let <$ keyword "let" <*> pVarName <* keyword "=" 
-               *> pTerm <* keyword "in" *> pTerm <* keyword "ni"
+    pLet = Let <$> pSymbol "let" *> pVarName <* pSymbol "="
+               *> pTerm <* pSymbol "in" *> pTerm <* pSymbol "ni"
 
-    keyword w = pMunch ws *> pToken w <* pMunch ws
-
-    parens p = pSymbol "(" *> p <* pSymbol ")"
+    parens p = pLParen *> p <* pRParen
