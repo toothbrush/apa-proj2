5846b80d71810a133c7e8d7441dba1069b8e204e
Jurriën Stutterheim <j.stutterheim@me.com>
2011-06-15 17:38:33 +0200

    More parser refactoring

--- src/Parser/ParseRules.hs
+++ src/Parser/ParseRules.hs
@@ -1,32 +1,30 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Parser.ParseRules where
- 
-    import Text.ParserCombinators.UU
-    import Text.ParserCombinators.UU.Utils
-    import Text.ParserCombinators.UU.BasicInstances
-    import Parser.Base
 
-    pTerm :: Parser Term
-    pTerm =
-             pVar
-         <|> pLam
-         <|> pApp
-         <|> pLet
+import Text.ParserCombinators.UU
+import Text.ParserCombinators.UU.Utils
+import Text.ParserCombinators.UU.BasicInstances
+import Parser.Base
 
-    pVarName :: Parser String
-    pVarName = lexeme (pToken "$" *> pList (pLetter <|> pDigit))
+pTerm :: Parser Term
+pTerm =
+         pVar
+     <|> pLam
+     <|> pApp
+     <|> pLet
 
-    pVar :: Parser Term
-    pVar = Var <$> pVarName
+pVarName :: Parser String
+pVarName = lexeme (pToken "$" *> pList (pLetter <|> pDigit))
 
-    pLam :: Parser Term
-    pLam = Lam <$> (pSymbol "\\" *> pVarName) <*> (pSymbol "->" *> pTerm)
+pVar :: Parser Term
+pVar = Var <$> pVarName
 
-    pApp :: Parser Term
-    pApp = App <$> parens pTerm <*> parens pTerm
+pLam :: Parser Term
+pLam = Lam <$> (pSymbol "\\" *> pVarName) <*> (pSymbol "->" *> pTerm)
 
-    pLet :: Parser Term
-    pLet = Let <$> (pSymbol "let" *> pVarName <* pSymbol "=")
-               <*> pTerm <*> (pSymbol "in" *> pTerm <* pSymbol "ni")
+pApp :: Parser Term
+pApp = App <$> pParens pTerm <*> pParens pTerm
 
-    parens p = pLParen *> p <* pRParen
+pLet :: Parser Term
+pLet = Let <$> (pSymbol "let" *> pVarName <* pSymbol "=")
+           <*> pTerm <*> (pSymbol "in" *> pTerm <* pSymbol "ni")
