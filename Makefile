d2e81f46a054586592bd0181552e58c8f1cb6e6d
Paul van der Walt <paul@denknerd.org>
2011-06-15 18:17:17 +0100

    compiles again, now to figure out why the ATerm cannot be parsed

--- Makefile
+++ Makefile
@@ -10,20 +10,25 @@ dist:
 install: ag
 	cabal install
 
-ag: src/Base.ag
+ag: src/Base.ag \
+	src/Infer/Infer.ag \
+	src/Infer/Typed.ag
 	uuagc -dcfws --self --genlinepragmas src/Base.ag
+	uuagc -dcfws --self --genlinepragmas src/Infer/Typed.ag
 
 doc: ag
 	cabal configure
 	cabal haddock --executables
 
 clean:
-	-rm src/Parser/Base.hs
+	-rm src/Base.hs \
+		src/Infer/Infer.hs
 	cabal clean
 
 lint:
 	hlint \
-		src/CLIFuncs.hs \
+		src/Parser.hs \
+		src/TreeInstances.hs
 	
 #churn:
 #	hg churn --aliases authormap
--- src/TreeInstances.hs
+++ src/TreeInstances.hs
@@ -2,9 +2,10 @@ module TreeInstances where
 
 import Base
 import Control.Applicative
-import CCO.Tree                   (Tree (fromTree, toTree))
-import qualified CCO.Tree as T    (ATerm (App))
-import CCO.Tree.Parser            (parseTree, app, arg)
+import CCO.Tree                    (Tree (fromTree, toTree))
+import qualified CCO.Tree as T     (ATerm (App))
+import CCO.Tree.Parser             (parseTree, app, arg)
+import qualified Infer.Typed as SF (Tm(..),Ty(..))
 
 instance Tree Term where
   fromTree (Var x)       = T.App "Var" [fromTree x]
@@ -16,3 +17,26 @@ instance Tree Term where
                      , app "App" (App <$> arg <*> arg        )
                      , app "Let" (Let <$> arg <*> arg <*> arg)
                      ]
+
+instance Tree SF.Ty where
+  fromTree (SF.TyVar a)      = T.App "TyVar" [fromTree a]
+  fromTree (SF.Arr ty1 ty2)  = T.App "Arr" [fromTree ty1, fromTree ty2]
+  fromTree (SF.Forall a ty1) = T.App "Forall" [fromTree a, fromTree ty1]
+  toTree = parseTree [ app "TyVar"  (SF.TyVar  <$> arg        )
+                     , app "Arr"    (SF.Arr    <$> arg <*> arg)
+                     , app "Forall" (SF.Forall <$> arg <*> arg)
+                     ]
+
+instance Tree SF.Tm where
+  fromTree (SF.Var x)       = T.App "Var"   [fromTree x]
+  fromTree (SF.Lam x ty t1) = T.App "Lam"   [fromTree x, fromTree ty, fromTree t1]
+  fromTree (SF.App t1 t2)   = T.App "App"   [fromTree t1, fromTree t2]
+  fromTree (SF.TyLam a t1)  = T.App "TyLam" [fromTree a, fromTree t1]
+  fromTree (SF.TyApp t1 ty) = T.App "TyApp" [fromTree t1, fromTree ty]
+  toTree = parseTree [ app "Var"   (SF.Var <$> arg                )
+                     , app "Lam"   (SF.Lam <$> arg <*> arg <*> arg)
+                     , app "App"   (SF.App <$> arg <*> arg        )
+                     , app "TyLam" (SF.TyLam <$> arg <*> arg      )
+                     , app "TyApp" (SF.TyApp <$> arg <*> arg      )
+                     ]
+
