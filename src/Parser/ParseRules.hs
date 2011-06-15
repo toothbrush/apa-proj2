ed78c837d19aab6f6bd4fddc6b8a00ce9273fa31
Paul van der Walt <paul@denknerd.org>
2011-06-15 15:59:53 +0100

    app works, let not yet

--- src/Parser/ParseRules.hs
+++ src/Parser/ParseRules.hs
@@ -7,19 +7,35 @@ module Parser.ParseRules where
     import Parser.Base
 
     pTerm :: Parser Term
-    pTerm =  (pVar 
-         <|> pLam)
-       --  <|> pApp
-       --  <|> pLet
-          <* pMaybe (pLF)
+    pTerm = (
+             pVar 
+         <|> pLam
+         <|> pApp
+         <|> pLet
+            ) 
+          <* pMunch ws
 
     pVarName :: Parser String
     pVarName = pToken "$" *> pMunch (\x -> elem x (['a'..'z']++['0'..'9']))
 
+    -- | ws tells us if a char is whitespace
+    ws :: Char -> Bool
+    ws x = elem x "\n\r\t "
+
     pVar :: Parser Term
-    pVar = Var <$> pVarName
+    pVar = Var <$> pVarName <* pMunch ws
 
     pLam :: Parser Term
-    pLam = Lam <$ pToken "\\ " <*> pVarName <* pToken "->" <*> pTerm
-    pApp = undefined
-    pLet = undefined
+    pLam = Lam <$ keyword "\\" <*> pVarName <* keyword "->" 
+               <*> pTerm <* pMunch ws
+
+    pApp :: Parser Term
+    pApp = App <$> parens pTerm <*> parens pTerm
+
+    pLet :: Parser Term
+    pLet = Let <$ keyword "let" <*> pVarName <* keyword "=" 
+               *> pTerm <* keyword "in" *> pTerm <* keyword "ni"
+
+    keyword w = pMunch ws *> pToken w <* pMunch ws
+
+    parens p = pSymbol "(" *> p <* pSymbol ")"
