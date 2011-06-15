724e8fe360c26100276b30561441893e6b889039
Paul van der Walt <paul@denknerd.org>
2011-06-15 14:42:34 +0100

    minor details

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,15 @@
 module Main where
     
-    main = putStrLn "test"
+    import Parser.Base
+    import Parser.PrettyPrint
+    import Text.ParserCombinators.UU
+    import System
+    
+    main :: IO ()
+    main = do
+      putStrLn "Parser for FUN language.\n"
+      (fileName:_) <- getArgs
+      putStrLn $ show $fileToTerm fileName
+
+    fileToTerm :: String -> Term
+    fileToTerm fileName = Var "s"
