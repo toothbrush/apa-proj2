8e3c9a983b574df8fecd9619324551705c21a2c6
Jurriën Stutterheim <j.stutterheim@me.com>
2011-06-17 18:11:07 +0200

    Re-fix main.. more or less

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,26 @@
 module Main where
 
-import CCO.HM
+import CCO.Component    (printer, ioWrap)
+import CCO.HM           (parser)
+import CCO.HM.AG
+import Control.Arrow    (arr, (>>>))
+import CCO.Tree                   (Tree (fromTree, toTree))
+import qualified CCO.Tree as T    (ATerm (App))
+import CCO.Tree.Parser            (parseTree, app, arg)
+import Control.Applicative        (Applicative ((<*>)), (<$>))
 
-main = putStrLn "Type and Effect System"
+
+instance Tree Tm where
+  fromTree (Var x)       = T.App "Var" [fromTree x]
+  fromTree (Lam x t1)    = T.App "Lam" [fromTree x, fromTree t1]
+  fromTree (App t1 t2)   = T.App "App" [fromTree t1, fromTree t2]
+  fromTree (Let x t1 t2) = T.App "Let" [fromTree x, fromTree t1, fromTree t2]
+
+  toTree = parseTree [ app "Var" (Var <$> arg                )
+                     , app "Lam" (Lam <$> arg <*> arg        )
+                     , app "App" (App <$> arg <*> arg        )
+                     , app "Let" (Let <$> arg <*> arg <*> arg)
+                     ]
+
+
+main = ioWrap (parser >>> arr fromTree >>> printer)
