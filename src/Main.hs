d19487dd704a70fee28723698f660211839fe58c
Jurriën Stutterheim <j.stutterheim@me.com>
2011-06-17 20:50:07 +0200

    Hack to make infered types visible

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import CCO.Component    (printer, ioWrap)
+import CCO.Component    (printer, ioWrap, component)
 import CCO.HM           (parser)
 import CCO.HM.AG
 import Control.Arrow    (arr, (>>>))
@@ -8,7 +8,9 @@ import CCO.Tree                   (Tree (fromTree, toTree))
 import qualified CCO.Tree as T    (ATerm (App))
 import CCO.Tree.Parser            (parseTree, app, arg)
 import Control.Applicative        (Applicative ((<*>)), (<$>))
-
+import Data.Map (Map)
+import qualified Data.Map as DM
+import Debug.Trace
 
 instance Tree Tm where
   fromTree (Var x)       = T.App "Var" [fromTree x]
@@ -22,5 +24,27 @@ instance Tree Tm where
                      , app "Let" (Let <$> arg <*> arg <*> arg)
                      ]
 
+convertAndType = component (return . doConversion)
 
-main = ioWrap (parser >>> arr fromTree >>> printer)
+doConversion :: Tm -> Ty
+doConversion t = let inferredType = inferredType_Syn_Tm (wrap_Tm (sem_Tm t) inherit)
+                     {- annotated    = annotated_Syn_Tm (wrap_Tm (sem_Tm t) inherit)-}
+                     -- we must generalise one more time, to figure out which
+                     -- TyLam's must be prepended. 
+                     {- (ty', coercion) = generalise [] inferredType-}
+                 {- in  trace ("Type of entire expression: \n  :: "++show ty'++"\n") -}
+                     {- (coercion annotated)-}
+                in inferredType
+-- | The top-level inherited attribute to be passed to an attribute grammar
+-- for System F. In our case, we want to start with an empty type
+-- environment, and a variable counter of 0 (used for generating fresh
+-- type variables).
+inherit :: Inh_Tm
+inherit = Inh_Tm { typeEnvironment_Inh_Tm = DM.empty -- Start with empty environment.
+                 , counter_Inh_Tm = 0          -- Start variable-name-seed at 0.
+                 }
+
+
+printer' = component (return . show)
+
+main = ioWrap (parser >>> convertAndType >>> printer')
