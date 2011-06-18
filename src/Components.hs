d206c90ffc523d49a0ffc8949a48f1737e1a2c2d
Ruben de Gooijer <rubendegooijer@gmail.com>
2011-06-18 23:53:57 +0200

    replaced cco parser with haskell-src-exts parser, adjusted examples

---
+++ src/Components.hs
@@ -0,0 +1,47 @@
+module Components where
+import Language.Haskell.Exts.Parser
+import qualified Language.Haskell.Exts.Syntax as H
+import qualified Data.Map as DM
+import APA2.AG
+
+inferTypes :: MH -> Ty
+inferTypes tm = 
+  let inheritAttr = Inh_MH { typeEnvironment_Inh_MH = DM.empty
+                           , counter_Inh_MH = 0
+                           }
+  in algW_Syn_MH (wrap_MH (sem_MH tm) inheritAttr)
+
+parseProgram :: String -> MH 
+parseProgram = translate . fromParseResult . parseExp 
+
+translate = hExpr
+  where
+  hExpr (H.Lit (H.Int i)) = VInt i
+  hExpr (H.Con (H.UnQual (H.Ident x))) | x == "True"  = VBool True
+                                       | x == "False" = VBool False
+
+  hExpr (H.Var n) = Var (hQName n)
+
+  hExpr (H.App e e') = App (hExpr e) (hExpr e')
+
+  hExpr (H.Let (H.BDecls (d:_)) e) = hDecl d (hExpr e)
+
+  hExpr (H.If c e e') = If (hExpr c) (hExpr e) (hExpr e')
+
+  hExpr (H.Lambda _ pt e) = toLambda pt (hExpr e)
+
+  hExpr (H.Paren e) = hExpr e
+
+  hPat (H.PVar n) = hName n
+  
+  hDecl (H.PatBind _ (H.PVar (H.Ident x)) Nothing (H.UnGuardedRhs e) _) = Let x (hExpr e)
+  hDecl (H.FunBind (m:_)) = hMatch m
+
+  hMatch (H.Match _ n pts nothing (H.UnGuardedRhs e) _) = Let (hName n) (toLambda pts (hExpr e))
+
+  hQName (H.UnQual (H.Ident x)) = x
+
+  hName (H.Ident x) = x
+
+  toLambda = \p e -> foldr (\(H.PVar n) -> Lambda $ hName n) e p
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,50 +1,8 @@
 module Main where
+import Components
 
-import CCO.Component    (printer, ioWrap, component)
-import APA2.HM           (parser)
-import APA2.AG
-import Control.Arrow    (arr, (>>>))
-import CCO.Tree                   (Tree (fromTree, toTree))
-import qualified CCO.Tree as T    (ATerm (App))
-import CCO.Tree.Parser            (parseTree, app, arg)
-import Control.Applicative        (Applicative ((<*>)), (<$>))
-import Data.Map (Map)
-import qualified Data.Map as DM
-import Debug.Trace
+main = 
+  do
+    input <- getContents
+    putStrLn $ ( (show . inferTypes . parseProgram) input )
 
-instance Tree Tm where
-  fromTree (Var x)       = T.App "Var" [fromTree x]
-  fromTree (Lam x t1)    = T.App "Lam" [fromTree x, fromTree t1]
-  fromTree (App t1 t2)   = T.App "App" [fromTree t1, fromTree t2]
-  fromTree (Let x t1 t2) = T.App "Let" [fromTree x, fromTree t1, fromTree t2]
-
-  toTree = parseTree [ app "Var" (Var <$> arg                )
-                     , app "Lam" (Lam <$> arg <*> arg        )
-                     , app "App" (App <$> arg <*> arg        )
-                     , app "Let" (Let <$> arg <*> arg <*> arg)
-                     ]
-
-convertAndType = component (return . doConversion)
-
-doConversion :: Tm -> Ty
-doConversion t = let inferredType = algW_Syn_Tm (wrap_Tm (sem_Tm t) inherit)
-                     {- annotated    = annotated_Syn_Tm (wrap_Tm (sem_Tm t) inherit)-}
-                     -- we must generalise one more time, to figure out which
-                     -- TyLam's must be prepended. 
-                     {- (ty', coercion) = generalise [] inferredType-}
-                 {- in  trace ("Type of entire expression: \n  :: "++show ty'++"\n") -}
-                     {- (coercion annotated)-}
-                in inferredType
--- | The top-level inherited attribute to be passed to an attribute grammar
--- for System F. In our case, we want to start with an empty type
--- environment, and a variable counter of 0 (used for generating fresh
--- type variables).
-inherit :: Inh_Tm
-inherit = Inh_Tm { typeEnvironment_Inh_Tm = DM.empty -- Start with empty environment.
-                 , counter_Inh_Tm = 0          -- Start variable-name-seed at 0.
-                 }
-
-
-printer' = component (return . show)
-
-main = ioWrap (parser >>> convertAndType >>> printer')
