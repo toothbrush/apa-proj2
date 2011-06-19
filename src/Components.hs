b23a8f10ff52c7e58a20e26526e5c97198d9202d
Ruben de Gooijer <rubendegooijer@gmail.com>
2011-06-19 21:54:16 +0200

    removed warnings, replaced algW with ty which makes more sense

--- src/Components.hs
+++ src/Components.hs
@@ -5,8 +5,8 @@ import qualified Data.Map as DM
 import APA2.AG
 import Data.Generics.Schemes
 import Data.Generics.Aliases
-import Debug.Trace
 
+initialInheritedAttributes :: Inh_MH
 initialInheritedAttributes = 
   Inh_MH { typeEnvironment_Inh_MH = DM.empty
          , counter_Inh_MH = 0
@@ -15,7 +15,7 @@ initialInheritedAttributes =
 w :: MH -> (TySubst, Ty)
 w tm =
   (substitution_Syn_MH (wrap_MH (sem_MH tm) initialInheritedAttributes)
-  ,algW_Syn_MH (wrap_MH (sem_MH tm) initialInheritedAttributes)
+  ,ty_Syn_MH (wrap_MH (sem_MH tm) initialInheritedAttributes)
   )
 
 inferTypes :: MH -> Ty
@@ -62,13 +62,17 @@ translate = hExpr
   hExpr (H.InfixApp e (H.QConOp (H.Special H.Cons)) (H.List [])) = Cons (hExpr e) Nil
   hExpr (H.InfixApp e (H.QConOp (H.Special H.Cons)) e') = Cons (hExpr e) (hExpr e')
 
-  hPat (H.PVar n) = hName n
+  hExpr e = notSupported e
 
   hQName (H.UnQual (H.Ident x)) = x
+  hQName e = notSupported e
 
   hName (H.Ident x) = x
+  hName e           = notSupported e
 
-  toLambda = foldr (\(H.PVar n) -> Lambda $ hName n)
+  toLambda = foldr o
+    where o (H.PVar n) = Lambda $ hName n
+          o _          = id
 
   isRecursiveLet :: String -> H.Exp -> Bool
   isRecursiveLet f e =
@@ -80,12 +84,12 @@ translate = hExpr
   removeShadowingExpressions :: String -> H.Exp -> H.Exp
   removeShadowingExpressions f = everywhere' (mkT letOrLam)
     where
-    cond x f l
+    cond x l
       | x == f    = emptyExp
       | otherwise = l
 
-    letOrLam l@(H.Let (H.BDecls ((H.FunBind ((H.Match _ (H.Ident x) _ _ _ _) :_ )) : _)) _) = cond x f l
-    letOrLam l@(H.Let (H.BDecls ((H.PatBind _ (H.PVar (H.Ident x)) _ _ _) : _)) _)          = cond x f l
+    letOrLam l@(H.Let (H.BDecls ((H.FunBind ((H.Match _ (H.Ident x) _ _ _ _) :_ )) : _)) _) = cond x l
+    letOrLam l@(H.Let (H.BDecls ((H.PatBind _ (H.PVar (H.Ident x)) _ _ _) : _)) _)          = cond x l
 
     letOrLam l@(H.Lambda _ pt _) =
       case [f | H.PVar n <- pt, hName n == f] of
@@ -93,5 +97,7 @@ translate = hExpr
         _   -> emptyExp
 
     letOrLam l = l
+
+  notSupported e = error ("not supported" ++ show e)
 
   emptyExp = H.Var (H.UnQual $ H.Ident "$%")
