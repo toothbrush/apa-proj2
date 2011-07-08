e168f982ffc647e10149d3f61033e674c824fed7
Jurriën Stutterheim <j.stutterheim@me.com>
2011-07-08 21:56:57 +0200

    Help for Main

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ import System.Environment ( getArgs )
 import Language.Haskell.Exts.Parser
 import Components
 
+-- | Main
 main :: IO ()
 main = do
   args  <- getArgs
@@ -16,6 +17,16 @@ main = do
             _          -> do putStrLn ("Argument not recognized")
                              putStrLn usage
 
+-- | Default action if no argument is provided to the executable. It parses the program, executes the analysis and prints the results.
+doDefault :: String -> IO ()
 doDefault input = (analysisResult . parseProgram)  input
 
-usage = "no usage description yet"  
+-- | The help message
+usage :: String
+usage = "Usage: \n\n" ++
+        "cfa cmd\n\n" ++
+        "Where cmd is one of:\n\n" ++
+        "debug       Shows debug output\n" ++
+        "parse-mh    Parses the untyped lambda calculus and shows the output\n" ++
+        "parse-hs    Parses Haskell source code and shows the output\n" ++
+        "help        Shows this help text"
